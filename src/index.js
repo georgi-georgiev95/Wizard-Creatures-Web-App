feb3a965bd235bcdb2feb63b9d100f6a0f89bfc2
Georgi957 <gesh007@abv.bg>
2024-01-29 11:51:28 +0200

    Extract magic strings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
-
+const { PORT } = require('./helpers/constants');
 const app = express();
 
 expressConfig(app);
 
 
-app.listen(3000, () => console.log(`App is listening on port 3000`));
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
