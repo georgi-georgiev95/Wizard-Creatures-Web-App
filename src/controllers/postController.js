e3a25760a6fb8c0095a3bcf9a17fc19d6e2957d3
Georgi957 <gesh007@abv.bg>
2024-01-29 15:40:29 +0200

    Show static create page

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 router.get('/catalog', (req, res) => {
     res.render('posts/catalog');
+});
+
+router.get('/create', (req, res) => {
+    res.render('posts/create');
 })
 
 module.exports = router;
