8d349114fb9ae119eae4a81cc2c9f57acc8cfc12
Georgi957 <gesh007@abv.bg>
2024-01-29 15:34:19 +0200

    Add static catalog page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const postController = require('./controllers/postController');
 
 router.use(homeController);
 router.use('/user', userController);
+router.use('/posts', postController);
 router.use('*', (req, res) => {
     res.redirect('/404');
 })
