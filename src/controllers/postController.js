611ee0714f5cffda49a6a981298451eb030ba616
Georgi957 <gesh007@abv.bg>
2024-01-29 15:51:02 +0200

    Create new post in DB

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 
+const postManager = require('../managers/postManager');
+
 router.get('/catalog', (req, res) => {
     res.render('posts/catalog');
 });
 
 router.get('/create', (req, res) => {
     res.render('posts/create');
+});
+
+router.post('/create', async (req, res) => {
+    const postData = {
+        ...req.body,
+        owner: req.user._id
+    }
+
+    await postManager.create(postData);
+
+    res.redirect('/posts/catalog')
 })
 
 module.exports = router;
