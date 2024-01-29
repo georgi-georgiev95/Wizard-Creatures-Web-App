1e6b24542ba2ff122664336d7354543c4b34ed8f
Georgi957 <gesh007@abv.bg>
2024-01-29 16:24:57 +0200

    Display proper buttons

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -20,6 +20,21 @@ router.post('/create', async (req, res) => {
     await postManager.create(postData);
 
     res.redirect('/posts/catalog')
+});
+
+router.get('/details/:postId', async (req, res) => {
+    const id = req.params.postId;
+    const postData = await postManager.getOne(id).lean();
+
+    let isOwner = req.user?._id == postData.owner._id;
+    let isUser = req.user?._id;
+    let hasVoted = postData.votes.filter(x => x._id == req.user._id).length !== 0 ? true : false;
+
+    console.log(isOwner);
+    console.log(isUser);
+    console.log(hasVoted);
+
+    res.render('posts/details', { postData, isOwner, isUser, hasVoted });
 })
 
 module.exports = router;
--- src/managers/postManager.js
+++ src/managers/postManager.js
@@ -3,3 +3,5 @@ const Post = require('../models/Post');
 exports.create = (postData) => Post.create(postData); 
 
 exports.getAll = () => Post.find();
+
+exports.getOne = (id) => Post.findById(id).populate('owner');
