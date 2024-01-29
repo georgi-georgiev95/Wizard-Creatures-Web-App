bffbeeb5d59f103fd00a5e6d9dc2ddc8b8d98b79
Georgi957 <gesh007@abv.bg>
2024-01-29 11:50:13 +0200

    Setup router

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const router = require('../router');
 
 module.exports = (app) => {
     app.use(express.urlencoded({ extended: false }));
+    app.use(router);
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const app = express();
 
 expressConfig(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello from express');
-});
-
 
 app.listen(3000, () => console.log(`App is listening on port 3000`));
 
